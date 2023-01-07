89be897f070dd5b656d4c27c83bc7a4fe12a6894
brettl1991 <brettl1991@gmail.com>
2023-01-07 19:24:57 +0000

    Remix projext using TypeScript, HTML, and CSS

---
+++ app/routes/contact.tsx
@@ -0,0 +1,5 @@
+const C = () => {
+  return <div>TEST</div>;
+};
+
+export default C;
---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+condt TeslaUIPOCV6=()=>{return(
+  <div>
+    <h1>Tesla Project</h1>
+    <p>Agnes fun solution</p>
+  </div>
+)}
+
+
+export {TeslaUIPOCV6}
