b8e17bec0212d485477f00acb77c72f8d279bce7
brettl1991 <brettl1991@gmail.com>
2023-01-14 17:02:33 +0000

    feat: get the final looks

--- app/component/chevron/chevron.tsx
+++ app/component/chevron/chevron.tsx
@@ -1,6 +1,7 @@
 import type { LinksFunction } from "@remix-run/react/dist/routeModules";
 
 import chevronStyle from "./style/chevron.css";
+import type * as T from "./chevron.type";
 
 const chevronLink: LinksFunction = () => [
   {
@@ -9,16 +10,11 @@ const chevronLink: LinksFunction = () => [
   },
 ];
 
-const Chevron = () => {
+const Chevron = ({ rotate }: T.Chevron) => {
   return (
-    <>
-      <div data-chevron>
-        <img src="/chevron.svg" alt="Arrow down" width={24} />
-      </div>
-      {/* <div data-chevron data-rotate="180">
-        <img src="/chevron.svg" alt="Arrow down" width={24} />
-      </div> */}
-    </>
+    <div data-chevron data-rotate={rotate}>
+      <img src="/chevron.svg" alt="Arrow down" width={24} />
+    </div>
   );
 };
 
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -21,11 +21,57 @@ export const links: LinksFunction = () => [
   ...boxLink(),
 ];
 
+const missingDataMap = {
+  "model-3": {
+    title: "Model 3",
+    text: "Order Online for",
+    link: "Touchless Delivery",
+    buttonList: ["CUSTOM ORDER", "EXISTING INVENTORY"],
+  },
+
+  "model-y": {
+    title: "Model Y",
+    text: "Order Online for",
+    link: "Touchless Delivery",
+    buttonList: ["CUSTOM ORDER", "EXISTING INVENTORY"],
+  },
+
+  "model-s": {
+    title: "Model S",
+    text: "Order Online for",
+    link: "Touchless Delivery",
+    buttonList: ["CUSTOM ORDER", "EXISTING INVENTORY"],
+  },
+
+  "model-x": {
+    title: "Model X",
+    text: "Order Online for",
+    link: "Touchless Delivery",
+    buttonList: ["CUSTOM ORDER", "EXISTING INVENTORY"],
+  },
+
+  "solar-panels": {
+    title: "Solar Panels",
+    text: "Lowest Cost Solar Panels in America",
+    buttonList: ["ORDER NOW", "LEARN MORE"],
+  },
+  "solar-roof": {
+    title: "Solar Roof",
+    text: "Order Online for",
+    buttonList: ["ORDER NOW", "LEARN MORE"],
+  },
+
+  accessories: {
+    title: "Accessories",
+    buttonList: ["SHOP NOW"],
+  },
+};
+
 const TeslaUIPOCV6 = () => {
   console.log(sectionList);
   return (
     <>
-      {sectionList.map((section) => {
+      {sectionList.map((section, k) => {
         return (
           <Box
             key={section}
@@ -36,16 +82,21 @@ const TeslaUIPOCV6 = () => {
             gap
           >
             <Box display="grid">
-              <Text title>{section}</Text>
-              <Text>Order Online for</Text>
-              <Text underline>Touchless Delivery</Text>
+              <Text title>{missingDataMap[section].title}</Text>
+              <Text>{missingDataMap[section]?.text}</Text>
+              {missingDataMap[section]?.link && <Text underline>{}</Text>}
             </Box>
             <Box vertical display="flex" gap align="center" justify="center">
               <Box display="flex" gap vertical>
-                <Button>CUSTOM ORDER</Button>
-                <Button>EXISTING INVENTORY</Button>
+                {missingDataMap[section].buttonList.map((buttonText, key) => {
+                  return (
+                    <Button light={!!key} key={Text}>
+                      {buttonText}
+                    </Button>
+                  );
+                })}
               </Box>
-              <Chevron />
+              <Chevron rotate={k + 1 === sectionList.length ? "180" : ""} />
             </Box>
           </Box>
         );
